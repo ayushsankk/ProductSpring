5946b01db94ff49f3e716ffd7bf6cae5af6bf043
ayushsankk <ayushsankk17@gmail.com>
2023-05-22 11:32:10 +0530

    Adding Spring JPA support

---
+++ src/main/java/com/telusko/ProductSpring/ProductSpringApplication.java
@@ -0,0 +1,68 @@
+package com.telusko.ProductSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+@SpringBootApplication
+public class ProductSpringApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(ProductSpringApplication.class, args);
+
+		ProductService service =context.getBean(ProductService.class);
+
+		// hey service, add the products..
+
+//		service.addProduct(new Product("Type C", "Cable", "Black Drawer", 2024));
+//		service.addProduct(new Product("Mac Studio", "Computer", "White Table", 2025));
+//		service.addProduct(new Product("Focusrite Mixer", "Audio System", "White Table", 2025));
+//		service.addProduct(new Product("Asus Vivobook", "Laptop", "Brown Drawer", 2021));
+//		service.addProduct(new Product("Asus Rog", "Laptop", "Black Table", 2021));
+//		service.addProduct(new Product("Macbook pro", "Laptop", "Brown Drawer", 2022));
+//		service.addProduct(new Product("Wacom Pad", "Writing Pad", "Black Drawer", 2020));
+//		service.addProduct(new Product("Apple Keyboard", "Keyboard", "White Table", 2022));
+//		service.addProduct(new Product("Logitech Keyboard", "Keyboard", "Black Table", 2024));
+//		service.addProduct(new Product("Hdmi cable", "Cable", "Black Drawer", 2024));
+//		service.addProduct(new Product("Java Black Book", "Cable", "Shelf", 2024));
+//		service.addProduct(new Product("Logi Mouse", "Mouse", "Black Table", 2022));
+//		service.addProduct(new Product("Apple Mouse", "Mouse", "White Table", 2022));
+//		service.addProduct(new Product("Lenovo Mouse", "Mouse", "Black Drawer", 2022));
+//		service.addProduct(new Product("BlackBeast", "Computer", "White Table", 2020));
+
+		List<Product> products = service.getAllProducts();
+		for(Product p : products){
+			System.out.println(p);
+		}
+		System.out.println("==============================================");
+
+		System.out.println("a Particular product");
+
+		Product p = service.getProduct("Logi Mouse");
+		System.out.println(p);
+
+		System.out.println("==============================================");
+		System.out.println("a Particular text");
+
+		List<Product> prods = service.getProductWithText("black");
+		for(Product product : prods){
+			System.out.println(product);
+		}
+
+		System.out.println("==============================================");
+		System.out.println("Get Products by a particular place");
+		List<Product> prodsByPlace = service.getProductByPlace("White table");
+		for(Product product : prodsByPlace){
+			System.out.println(product);
+		}
+
+		System.out.println("==============================================");
+		System.out.println("Get Products that are out of warranty");
+		List<Product> outOfWarrantyProducts = service.getOutOfWarrantyProducts();
+		outOfWarrantyProducts.forEach(product -> System.out.println(product));
+
+	}
+
+}
